06eb9a016bbb42fceff9bb4f7047216fdeb45a5d
Ilya Petrov <dnamix1@gmail.com>
2019-12-17 15:27:03 +0300

    post feed is half-done with getPhoto and createPost, getPosts controllers and routes

--- controllers/post.js
+++ controllers/post.js
@@ -20,9 +20,10 @@ exports.postById = (req, res, next, id) => {
 exports.getPosts = (req, res) => {
   const posts = Post.find()
     .populate("postedBy", "_id name")
-    .select("_id title body")
+    .select("_id title body created")
+    .sort({ created: -1 })
     .then(posts => {
-      res.json({ posts });
+      res.json(posts);
     })
     .catch(err => console.log(err));
 };
@@ -107,4 +108,9 @@ exports.deletePost = (req, res) => {
         "Post deleted successfully...Please, write more. We are your fans"
     });
   });
+};
+
+exports.userPhoto = (req, res, next) => {
+  res.set("Content-Type", req.post.photo.contentType);
+  return res.send(req.post.photo.data);
 };
--- routes/post.js
+++ routes/post.js
@@ -7,7 +7,8 @@ const {
   postById,
   deletePost,
   isPoster,
-  updatePost
+  updatePost,
+  userPhoto
 } = require("../controllers/post");
 const { createPostValidator } = require("../validator");
 const { requireSignIn } = require("../controllers/auth");
@@ -15,7 +16,8 @@ const { userById } = require("../controllers/user");
 
 const router = express.Router();
 
-router.get("/posts", requireSignIn, getPosts);
+router.get("/posts", getPosts);
+
 router.post(
   "/post/new/:userId",
   requireSignIn,
@@ -27,7 +29,8 @@ router.put("/post/:postId", requireSignIn, isPoster, updatePost);
 router.delete("/post/:postId", requireSignIn, isPoster, deletePost);
 // any route containing userId our app will first execute findById();
 router.param("userId", userById);
-
+// photo
+router.get("/post/photo/:postId", userPhoto);
 // any route containing :postId, our app will first execute postById();
 router.param("postId", postById);
 
